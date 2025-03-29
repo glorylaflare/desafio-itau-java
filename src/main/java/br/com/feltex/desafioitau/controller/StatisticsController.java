1df8e37063f4384017d9b8a1ea040dba152511cd
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-29 18:19:16 -0300

    add StatisticsController file

---
+++ src/main/java/br/com/feltex/desafioitau/controller/StatisticsController.java
@@ -0,0 +1,26 @@
+package br.com.feltex.desafioitau.controller;
+
+import br.com.feltex.desafioitau.dto.StatisticDTO;
+import br.com.feltex.desafioitau.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.DoubleSummaryStatistics;
+
+@RestController
+@RequestMapping("/estatistica")
+public class StatisticsController {
+
+    @Autowired
+    private TransactionService service;
+
+    //Get
+    @GetMapping
+    private ResponseEntity<StatisticDTO> getStatistics() {
+        DoubleSummaryStatistics statistics = service.getStatistics();
+        return ResponseEntity.ok(new StatisticDTO(statistics));
+    }
+}
