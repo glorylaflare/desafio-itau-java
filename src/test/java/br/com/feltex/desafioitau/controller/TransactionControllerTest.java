e7e638a3bf724ab114dddb9be1814b07dfa49c29
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-30 23:18:24 -0300

    refactor TransactionControllerTest with new instance of controller

--- src/test/java/br/com/feltex/desafioitau/controller/TransactionControllerTest.java
+++ src/test/java/br/com/feltex/desafioitau/controller/TransactionControllerTest.java
@@ -1,6 +1,9 @@
 package br.com.feltex.desafioitau.controller;
 
 import br.com.feltex.desafioitau.dto.TransactionDTO;
+import br.com.feltex.desafioitau.service.TransactionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +14,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TransactionControllerTest {
 
-    private TransactionController controller = new TransactionController();
+    private TransactionController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new TransactionController();
+    }
 
     @Test
+    @DisplayName("Deve retornar status code 201 após criar uma transação")
     void retornarStatus201AposCriarTransacao() {
         TransactionDTO dto = new TransactionDTO(100.0, OffsetDateTime.now());
 
@@ -22,6 +31,7 @@ class TransactionControllerTest {
     }
 
     @Test
+    @DisplayName("Deve retornar status code 422 quando uma transação for inconsistente")
     void retornarStatus422QuandoTransacaoForInconsistente() {
         TransactionDTO dto = new TransactionDTO(-100.0, OffsetDateTime.now());
 
@@ -30,6 +40,7 @@ class TransactionControllerTest {
     }
 
     @Test
+    @DisplayName("Deve retornar um status code 200 após deletar todas as transações em memória")
     void retornarStatus200AposDeletarTodasAsTransacoesEmMemoria() {
         ResponseEntity<Void> response = controller.deleteAll();
         assertEquals(HttpStatus.OK, response.getStatusCode());
