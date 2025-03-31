54fb223251a316d2bd9d82ed5626623113a60a68
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-31 18:15:05 -0300

    add some log infos about time

--- src/main/java/br/com/feltex/desafioitau/service/TransactionService.java
+++ src/main/java/br/com/feltex/desafioitau/service/TransactionService.java
@@ -1,10 +1,6 @@
 package br.com.feltex.desafioitau.service;
 
 import br.com.feltex.desafioitau.dto.TransactionDTO;
-import br.com.feltex.desafioitau.entity.Transaction;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -18,21 +14,27 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class TransactionService {
 
     private final Queue<TransactionDTO> transactions = new ConcurrentLinkedQueue<>();
+    long start = System.nanoTime();
 
     public void createTransaction(TransactionDTO transactionDTO) {
         transactions.add(transactionDTO);
+
+        long end = System.nanoTime();
         log.info("Nova transação adicionada: {}", transactionDTO);
+        log.info("Tempo para adicionar a transação: {} ms", (end - start) / 1_000_000);
     }
 
     public void deleteAll() {
         transactions.clear();
+        long end = System.nanoTime();
         log.info("Todas as transações foram removidas.");
+        log.info("Tempo para remover todas as transações: {} ms", (end - start) / 1_000_000);
     }
 
     public DoubleSummaryStatistics getStatistics() {
         OffsetDateTime offsetDateTime = OffsetDateTime.now();
         return transactions.stream()
-                //.filter(t -> t.dataHora().isAfter(offsetDateTime.minusSeconds(60)))
+                .filter(t -> t.dataHora().isAfter(offsetDateTime.minusSeconds(60)))
                 .mapToDouble(TransactionDTO::valor)
                 .summaryStatistics();
     }
